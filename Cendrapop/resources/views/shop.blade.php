d1937abe4045bc087a627ac568e322f2e785927a
EduardEsteba <38983280+EduEsteba@users.noreply.github.com>
2020-05-22 12:03:49 +0200

    Desplegable categories

--- Cendrapop/resources/views/shop.blade.php
+++ Cendrapop/resources/views/shop.blade.php
@@ -5,35 +5,33 @@
 	<div class="page-content-wrapper">
 		<div class="container">
 			<div class="row">
-				<div class="col-lg-3">
-					<div class="sidebar-container shop-sidebar-container">
+				<div class="col-lg-12">
 					<br>
 						<div class="single-sidebar-widget mb-3">
 							<h3 class="sidebar-title d-none d-lg-block">{{ __('Categories') }}</h3>
 							<nav class="navbar-expand-lg">
-								
-								<nav class="category-menu collapse navbar-collapse" id="categories-menu">
-									<ul class="category-list">
-										<li><a href="{{ route('shop') }}">{{ __('Totes') }}</a></li>
+							<div class="form-group">
+    								<select class="form-control" id="exampleFormControlSelect1">
 										@foreach ($categories as $category)
-											<li class=""><a href="{{ route('shop', ['category' => $category->id]) }}">{{ $category->title }}</a>
+											<option onClick="window.location = '{{ route('shop', ['category' => $category->id]) }}'"><a>{{ $category->title }}</option>
 										@endforeach
-									</ul>
-								</nav>
+    								</select>
+  							</div>
+								
+								
 							</nav>
 
 						</div>
-					</div>
 				</div>
 				<div class="col-lg-9">
 					<div class="shop-header mb-20">
 						<div class="row">
-							<div class="col-lg-6 col-md-6 col-sm-12 mb-sm-20 d-flex align-items-center">
+							<!--<div class="col-lg-6 col-md-6 col-sm-12 mb-sm-20 d-flex align-items-center">
 								<p class="result-show-message">{{ __('Mostrant') }} {{ $products->firstItem() }}–{{ $products->lastItem() }} de {{ $products->total() }} {{ __(' resultats') }}</p>
-							</div>
+							</div>-->
 							<div class="col-lg-6 col-md-6 col-sm-12 d-flex flex-column flex-sm-row justify-content-start justify-content-md-end align-items-sm-center">
 
-								<div class="sort-by-dropdown d-flex align-items-center mb-xs-10">
+								<div class="sort-by-dropdown d-flex mb-xs-10">
 									<div>
 										<strong>{{ __('Ordenar per preu:') }}</strong>
 										<a href="{{ route('shop', ['category'=> request()->category, 'sort' => 'low_high']) }}">{{ __('De petit a gran') }}</a> |
