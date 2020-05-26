d657a65fade0e0fd18629798f207853472f588aa
EduardEsteba <38983280+EduEsteba@users.noreply.github.com>
2020-05-26 16:21:03 +0200

    petis canvis

--- Cendrapop/resources/views/auth/login.blade.php
+++ Cendrapop/resources/views/auth/login.blade.php
@@ -33,14 +33,14 @@
 		<div class="row justify-content-center">
 			<div class="col-md-8">
 				<div class="card">
-					<div class="card-header text-center">{{ __('Login') }}</div>
+					<div class="card-header text-center">{{ __('Inici de Sessió') }}</div>
 
 					<div class="card-body">
 						<form method="POST" action="{{ route('login') }}">
 							@csrf
 
 							<div class="form-group row">
-								<label for="email" class="col-sm-4 col-form-label text-md-right">E-Mail Address</label>
+								<label for="email" class="col-sm-4 col-form-label text-md-right">E-Mail</label>
 
 								<div class="col-md-6">
 									<input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -60,7 +60,7 @@
 								@endif
 							</div>
 							<div class="form-group row">
-								<label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+								<label for="password" class="col-md-4 col-form-label text-md-right">Contrasenya</label>
 
 								<div class="col-md-6">
 									<input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
